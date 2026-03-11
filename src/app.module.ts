c187cf89b863400242e91475153234eb96b99b4a
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-03-11 10:11:35 -0300

    feat(prisma): add Prisma module and service with initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [ ConfigModule.forRoot({
       isGlobal: true,
-    }), UsersModule],
+    }), UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
