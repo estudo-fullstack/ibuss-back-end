bf5586f33fbc891a668d9ad004923c924f5ae81d
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-03-11 10:12:48 -0300

    feat(users): implement user creation logic and add bcrypt for password hashing

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,7 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  name: string;
+  cpf: string;
+  email: string;
+  phone_number: string;
+  password: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import bcrypt from 'bcrypt';
+//import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prismaService: PrismaService) {}
+  
+  async create(createUserDto: CreateUserDto) {
+    console.log(createUserDto)
+    const passwordHash = await bcrypt.hash(createUserDto.password, 10);
+    return this.prismaService.user.create({
+      data: {
+        ...createUserDto,
+        password: passwordHash,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
+  // findAll() {
+  //   return `This action returns all users`;
+  // }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} user`;
+  // }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
 }
