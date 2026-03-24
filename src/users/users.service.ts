187a6d721fa3dfe0cb3875a82cff028577f7a70b
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-03-24 13:22:56 -0300

    feat(user): add user update and password update functionality
    - Implemented base functionality to update user data through a dedicated route and a separate route for updating user password.
    - Created specific DTOs for each operation to ensure proper data validation and structure.
    
    Refs: 86afwctud, 86ag84k1t

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,35 +2,67 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import bcrypt from 'bcrypt';
-//import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
-    console.log(createUserDto);
     const passwordHash = await bcrypt.hash(createUserDto.password, 10);
     return this.prismaService.user.create({
       data: {
         ...createUserDto,
         password: passwordHash,
       },
+      select: {
+        name: true,
+        email: true,
+        phone_number: true,
+      },
     });
   }
-
-  // findAll() {
-  //   return `This action returns all users`;
-  // }
 
   // findOne(id: number) {
   //   return `This action returns a #${id} user`;
   // }
 
-  // update(id: number, updateUserDto: UpdateUserDto) {
-  //   return `This action updates a #${id} user`;
-  // }
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.prismaService.user.update({
+      data: {
+        ...updateUserDto,
+      },
+      where: {
+        id: id,
+      },
+      select: {
+        name: true,
+        email: true,
+        phone_number: true,
+      },
+    });
+  }
 
+  async updatePassword(
+    id: string,
+    updateUserPasswordDto: UpdateUserPasswordDto,
+  ) {
+    const passwordHash = await bcrypt.hash(updateUserPasswordDto.password, 10);
+    return this.prismaService.user.update({
+      data: {
+        password: passwordHash,
+      },
+      where: {
+        id: id,
+      },
+      select: {
+        name: true,
+        email: true,
+        phone_number: true,
+      },
+    });
+  }
   // remove(id: number) {
   //   return `This action removes a #${id} user`;
   // }
