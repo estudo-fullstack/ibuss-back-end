adf53685f3cafdf61f3ab78afa0aaf96ff1b176d
Fernanda Soares <fernanda.soares@elogroup.com.br>
2026-04-12 12:16:39 -0300

    fix: user:any in prisma.service file and add eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,6 +29,7 @@ export default [
       "@typescript-eslint/no-floating-promises": "warn",
       "@typescript-eslint/no-unsafe-argument": "warn",
       "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
       "prettier/prettier": ["error", { endOfLine: "auto", singleQuote: false }],
     },
