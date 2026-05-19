6f3128dfe5978b57abe9d3689e3bdcd5ba14095a
Andrea Santos <deasantos.dev@gmail.com>
2026-05-19 20:20:25 -0300

    refactor(users): remove tratamento de erros do Prisma do controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,8 +6,6 @@ import {
   Patch,
   Param,
   ParseUUIDPipe,
-  NotFoundException,
-  InternalServerErrorException,
   UseGuards
 } from "@nestjs/common";
 import { UsersService } from "./users.service";
@@ -15,10 +13,6 @@ import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { UpdateUserPasswordDto } from "./dto/update-user-password.dto";
 import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
-
-function isPrismaError(error: unknown): error is { code?: string } {
-  return typeof error === "object" && error !== null && "code" in error;
-}
 
 @Controller("users")
 export class UsersController {
@@ -32,20 +26,15 @@ export class UsersController {
   @UseGuards(JwtAuthGuard)
   @Get(":id")
   findOne(@Param("id", new ParseUUIDPipe()) id: string) {
-    try {
-      return this.usersService.findOne(id);
-    } catch (error) {
-      if (isPrismaError(error) && error.code === "P2025") {
-        throw new NotFoundException("User not found");
-      } else {
-        throw new InternalServerErrorException("An error occurred while updating the user");
-      }
-    }
+    return this.usersService.findOne(id);
   }
 
   @UseGuards(JwtAuthGuard)
   @Patch(":id")
-  update(@Param("id", new ParseUUIDPipe()) id: string, @Body() updateUserDto: UpdateUserDto) {
+  update(
+    @Param("id", new ParseUUIDPipe()) id: string,
+    @Body() updateUserDto: UpdateUserDto
+  ) {
     return this.usersService.update(id, updateUserDto);
   }
 
@@ -61,26 +50,12 @@ export class UsersController {
   @UseGuards(JwtAuthGuard)
   @Patch("deactivate/:id")
   deactivateUser(@Param("id", new ParseUUIDPipe()) id: string) {
-    try {
-      return this.usersService.deactivateUser(id);
-    } catch (error) {
-     if (isPrismaError(error) && error.code === "P2025") {
-        throw new NotFoundException("User not found");
-      } else throw new InternalServerErrorException("An error occurred while updating the user");
-    }
+    return this.usersService.deactivateUser(id);
   }
 
   @UseGuards(JwtAuthGuard)
   @Patch("suspend/:id")
   suspendUser(@Param("id", new ParseUUIDPipe()) id: string) {
-    try {
-      return this.usersService.suspendUser(id);
-    } catch (error) {
-      if (isPrismaError(error) && error.code === "P2025") {
-        throw new NotFoundException("User not found");
-      } else {
-        throw new InternalServerErrorException("An error occurred while updating the user");
-      }
-    }
+    return this.usersService.suspendUser(id);
   }
 }
