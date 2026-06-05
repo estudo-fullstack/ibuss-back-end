3879e0f37534b78f5b67127c801edcd271c28d06
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-06-05 08:44:08 -0300

    feat: add user ticket listing with filters

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -1,19 +1,23 @@
-import { Controller, Get, Post, Body, Param, UseGuards, Req } from "@nestjs/common";
+import { Controller, Get, Post, Body, UseGuards, Req, Query, ParseEnumPipe } from "@nestjs/common";
 import { TicketService } from "./ticket.service";
 import { CreateTicketDto } from "./dto/create-ticket.dto";
 
 import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 import type { Request } from "express";
+import { TicketStatusType } from "src/generated/prisma/enums";
 
 @UseGuards(JwtAuthGuard)
 @Controller("ticket")
 export class TicketController {
   constructor(private readonly ticketService: TicketService) {}
 
-  // @Get()
-  // findAll() {
-  //   return this.ticketService.findAll();
-  // }
+  @Get()
+  findAll(
+    @Req() req: Request,
+    @Query("status", new ParseEnumPipe(TicketStatusType)) status: TicketStatusType
+  ) {
+    return this.ticketService.findByUser(req.user!.id, status);
+  }
 
   // @Get(":id")
   // findOne(@Param("id") id: string) {
